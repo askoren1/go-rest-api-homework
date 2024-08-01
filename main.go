1f2878ec4258420005312e51bb2e17d045f5ffba
Koren Aleksandr <koren_aleksandr@mail.ru>
2024-08-01 12:10:38 +0300

    second iteration

--- main.go
+++ main.go
@@ -1,114 +1,137 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
 	"bytes"
-    "github.com/go-chi/chi/v5"
-) 
+	"encoding/json"
+	//"io"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"log"
+)
 
 type Task struct {
-    ID          string      `json:"id"`             // ID задачи
-    Description string      `json:"description"`    // Заголовок
-    Note        string      `json:"note"`           // Описание задачи
-    Applications []string    `json:"application"`    // Приложения, которыми будете пользоваться
+	ID           string   `json:"id"`          // ID задачи
+	Description  string   `json:"description"` // Заголовок
+	Note         string   `json:"note"`        // Описание задачи
+	Applications []string `json:"application"` // Приложения, которыми будете пользоваться
 }
 
 var tasks = map[string]Task{
-    "1": {
-        ID:          "1",
-        Description: "Сделать финальное задание темы REST API",
-        Note:        "Если сегодня сделаю, то завтра будет свободный день. Ура!",
-        Applications: []string{
-            "VS Code",
-            "Terminal",
-            "git",
-        },
-    },
-    "2": {
-        ID:          "2",
-        Description: "Протестировать финальное задание с помощью Postmen",
-        Note:        "Лучше это делать в процессе разработки, каждый раз, когда запускаешь сервер и проверяешь хендлер",
-        Applications: []string{
-            "VS Code",
-            "Terminal",
-            "git",
-            "Postman",
-        },
-    },
+	"1": {
+		ID:          "1",
+		Description: "Сделать финальное задание темы REST API",
+		Note:        "Если сегодня сделаю, то завтра будет свободный день. Ура!",
+		Applications: []string{
+			"VS Code",
+			"Terminal",
+			"git",
+		},
+	},
+	"2": {
+		ID:          "2",
+		Description: "Протестировать финальное задание с помощью Postmen",
+		Note:        "Лучше это делать в процессе разработки, каждый раз, когда запускаешь сервер и проверяешь хендлер",
+		Applications: []string{
+			"VS Code",
+			"Terminal",
+			"git",
+			"Postman",
+		},
+	},
 }
 
 func getTasks(w http.ResponseWriter, r *http.Request) {
-    // сериализуем данные из слайса tasks
-    resp, err := json.Marshal(tasks)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
 
-    // в заголовок записываем тип контента, у нас это данные в формате JSON
-    w.Header().Set("Content-Type", "application/json")
-    // так как все успешно, то статус OK
-    w.WriteHeader(http.StatusOK)
-    // записываем сериализованные в JSON данные в тело ответа
-    w.Write(resp)
-} 
+	// сериализуем данные из слайса tasks
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// в заголовок записываем тип контента, у нас это данные в формате JSON
+	w.Header().Set("Content-Type", "application/json")
+	// так как все успешно, то статус OK
+	w.WriteHeader(http.StatusOK)
+	// записываем сериализованные в JSON данные в тело ответа
+	if _,err:=w.Write(resp);err!=nil{
+		log.Println(err)
+	}
+	
+
+}
 
 func postTasks(w http.ResponseWriter, r *http.Request) {
-    var task Task
-    var buf bytes.Buffer
+	var task Task
+	var buf bytes.Buffer
 
-    _, err := buf.ReadFrom(r.Body)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    tasks[task.ID] = task
+	tasks[task.ID] = task
 
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 }
 
 func getTask(w http.ResponseWriter, r *http.Request) {
     id := chi.URLParam(r, "id")
 
-    task, ok := tasks[id]
-    if !ok {
-        http.Error(w, "Задача не найдена", http.StatusNoContent)
-        return
-    }
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusNoContent)
+		return
+	}
 
-    resp, err := json.Marshal(task)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write(resp)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if _,err:=w.Write(resp);err!=nil{
+		log.Println(err)
+	}
 }
 
+func delTask (w http.ResponseWriter, r *http.Request) {
+	// Получаем ID задачи из URL
+	id := chi.URLParam(r, "id")
+
+	// Проверяем, существует ли задача с таким ID
+	if _, ok := tasks[id]; !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Удаляем задачу из мапы
+	delete(tasks, id)
+
+	w.WriteHeader(http.StatusOK)
+
+}
 
 func main() {
-    r := chi.NewRouter()
+	r := chi.NewRouter()
 
-    r.Get("/tasks", getTasks)
+	r.Get("/tasks", getTasks)
 	r.Post("/tasks", postTasks)
 	r.Get("/tasks/{id}", getTask)
+	r.Delete("/tasks/{id}", delTask)
 
-
-
-    if err := http.ListenAndServe(":8080", r); err != nil {
-        fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
-        return
-    }
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
+	}
 }
-
